b352b0d513ab88cf7fed6837f612cbfb9489420c
Tomek Leks <tomek@el-jot.eu>
2024-06-12 11:04:13 +0200

    fixed rep password validation working on chanigng login page

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -136,7 +136,7 @@ export default function Login() {
                 })}
                 type="password"
                 placeholder="Repeat Password"
-                name="password"
+                name="repPassword"
                 className="border-4 bg-white text-black border-solid rounded-2xl max-w-[40rem] min-w-56 w-[30vw] max-h-12 min-h-8 h-[10vh] pl-4 pr-4 duration-300 focus:scale-110 focus:outline-none focus:bg-slate-800 focus:text-emerald-500 focus:border-slate-800"
               />
               <FormErrorParahraph errorObject={errors.repPassword} />
